718e0fc881c0bd8d80450bb9998de21909cc508e
vedhem <96457962+vedhem@users.noreply.github.com>
2022-10-26 20:17:14 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDwiLFpO45Yc1V7Zp7HaCWYXzSxbiTNIlI",
+authDomain: "spectagram-f7f38.firebaseapp.com",
+databaseURL: "https://spectagram-f7f38-default-rtdb.firebaseio.com",
+projectId: "spectagram-f7f38",
+storageBucket: "spectagram-f7f38.appspot.com",
+messagingSenderId: "919137908778",
+appId: "1:919137908778:web:f0e07dfa94a4ee6c9c6b53",
+measurementId: "G-Z11N5W0YN6"
+}
